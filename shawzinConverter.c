cde9c492df11519b2f48263d4c2e7bed384276c9
Vincent Liu <pkchu99@gmail.com>
2019-08-31 15:12:34 +1000

    Update shawzinConverter.c
    Support for chords, comments, formatted output.
    
    Fixed a timing issue.

--- shawzinConverter.c
+++ shawzinConverter.c
@@ -4,8 +4,11 @@
 #define MAX_STRING 2048
 #define SCALE_SIZE 12
 #define NOTE_LEN 4
+#define CHORD_LEN 64
 #define DEFAULT_BPM 120
 #define DEFAULT_CHARS_PER_BEAT 8
+
+#define FORMAT(x) if (formatted) { out[strlen(out)] = x; }
 
 static const char scalePentMin[SCALE_SIZE][NOTE_LEN + 1] = {"C","Eb","F","G","Bb","C^","Eb^","F^","G^","Bb^","Eb^^","F^^"};
 
@@ -19,32 +22,49 @@ static const char scaleMajor[SCALE_SIZE][NOTE_LEN + 1] = {"C","D","E","F","G","A
 
 static const char scaleMinor[SCALE_SIZE][NOTE_LEN + 1] = {"C","D","Eb","F","G","Ab","Bb","C^","D^","Eb^","F^","G^"};
 
-static const char scaleHirajoshi[SCALE_SIZE][NOTE_LEN + 1] = {"C","E","Gb","G","B","C","E","Gb^","G^","B^","C^","E^"};
+static const char scaleHirajoshi[SCALE_SIZE][NOTE_LEN + 1] = {"C","E","Gb","G","B","C^","E^","Gb^","G^","B^","C^^","E^^"};
 
 static const char scalePhrygian[SCALE_SIZE][NOTE_LEN + 1] = {"C","Db","E","F","G","Ab","Bb","C^","Db^","E^","F^","G^"};
 
 static const char scaleOutput[SCALE_SIZE] = {'B','C','E','J','K','M','R','S','U','h','i','k'};
 
+// convert base-10 to base-64
 char toBase64(int x) {
+    char c = EOF;
     if (0 <= x && x <= 25) {
-        return x + 'A';
+        c = x + 'A';
     } else if (26 <= x && x <= 51) {
-        return (x - 26) + 'a';
+        c = (x - 26) + 'a';
     } else if (52 <= x && x <= 61) {
-        return (x - 52) + '0';
+        c = (x - 52) + '0';
     } else if (x == 62) {
-        return '+';
+        c = '+';
     } else if (x == 63) {
-        return '/';
+        c = '/';
     } else {
-        puts("error");
-        return EOF;
+        puts("base-64 conversion failed");
+    }
+    return c;
+}
+
+// convert sharps to enharmonic equivalent
+// e.g. G# -> Ab
+void toEnharmonicFlat(char* note) {
+    if (note[1] == '#') {
+        note[1] = 'b';
+        char c = note[0];
+        if ('A' <= c && c < 'G') {
+            note[0] = c + 1;
+        } else if (c == 'G') {
+            note[0] = 'A';
+        }
     }
 }
 
 int indexOf(char* x, char* arr, int len) {
     int ret = -1;
     for (int i = 0; i < len; i++) {
+        // interpret arr as array of strings
         if (strcmp((arr + (NOTE_LEN + 1)*i), x) == 0) {
             ret = i;
         }
@@ -53,15 +73,26 @@ int indexOf(char* x, char* arr, int len) {
 }
 
 int main(int argc, char** argv) {
+    if (argc < 2) {
+        puts("Usage: shawzinconverter <input file> [-f]");
+        return 1;
+    }
     FILE* fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        printf("File \"%s\" not found.\n", argv[1]);
+        return 1;
+    }
 
+    int formatted = (argc > 2) ? !strcmp(argv[2], "-f") : 1;
+    int lineNo = 0;
     char out[MAX_STRING] = {'\0'};
     int scale;
-    double bpm;
     char* scaleNotes;
+    double bpm;
 
+    // get scale
     fscanf(fp, "%i", &scale);
-
+    lineNo++;
     switch (scale) {
     case 1: scaleNotes = (char*) scalePentMin;   break;
     case 2: scaleNotes = (char*) scalePentMaj;   break;
@@ -74,52 +105,80 @@ int main(int argc, char** argv) {
     default: puts("Invalid scale - must be an integer from 1 to 8"); return 1;
     }
     out[0] = '0' + scale;
+    FORMAT('\n');
 
+    // get bpm
     fscanf(fp, "%lf", &bpm);
+    lineNo++;
     if (bpm <= 0) {
         puts("Invalid BPM - must be positive.");
         return 1;
     }
 
-    double bps = bpm/60.0;
-    double charsPerBeat = DEFAULT_CHARS_PER_BEAT/(bpm/DEFAULT_BPM);
+    // calculate chars per beat
+    double charsPerBeat = DEFAULT_CHARS_PER_BEAT * (DEFAULT_BPM/bpm);
 
     char note[NOTE_LEN + 1] = {'\0'};
     char bar = 'A';
     double time = 0;
     int duration1 = 0;
     int duration2 = 1;
-    int lineNo = 0;
 
-    while (1) {
+    // iterate over each line of file
+    while (!feof(fp)) {
+        
         lineNo++;
-        int res = fscanf(fp, "%s %i/%i\n", note, &duration1, &duration2);
-        if (res == EOF) {
-            break;
-        } else if (res < 2) {
-            printf("Invalid line (line %i)\n", lineNo);
-            return 1;
-        }
-        int i = indexOf(note, scaleNotes, SCALE_SIZE);
-        if (i == -1) {
-            printf("Invalid note %s\n", note);
-            return 1;
-        }
 
-        out[strlen(out)] = scaleOutput[i];
-        out[strlen(out)] = bar;
-        out[strlen(out)] = toBase64((int) time);
+        // scan in all notes of a chord
+        do {
+            // read a note
+            if (fscanf(fp, "%s", note) < 1) {
+                // nothing read
+                break;
+            }
 
+            // find comments
+            if (note[0] == '#') {
+                // discard the rest of the line
+                while (fgetc(fp) != '\n');
+                break;
+            }
+
+            // find out which note is being read
+            toEnharmonicFlat(note);
+            int i = indexOf(note, scaleNotes, SCALE_SIZE);
+            if (i == -1) {
+                printf("Invalid note %s (line %i)\n", note, lineNo);
+                return 1;
+            }
+
+            // generate converted song string
+            out[strlen(out)] = scaleOutput[i];
+            out[strlen(out)] = bar;
+            out[strlen(out)] = toBase64((int) time);
+
+            // check if finished reading notes
+            // if no more notes, integers are read
+            // so res != 0 and loop ends
+        } while (fscanf(fp, "%i/%i", &duration1, &duration2) == 0);
+
+        FORMAT('\n');
+
+        // increment time offset
         time += ((double) duration1/(double) duration2) * charsPerBeat;
-        if (time > 63) {
-            time -= 63;
+        if (time >= 64) {
+            time -= 64;
             bar++;
         }
 
-        memset(note, 0, NOTE_LEN);
         duration1 = 0;
         duration2 = 1;
+
     }
-    printf("Song string: \n%s\n", out);
+
+    fclose(fp);
+
+    printf("%s\n", out);
+
     return 0;
 }
